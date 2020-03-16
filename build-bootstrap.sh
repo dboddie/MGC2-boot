c003e4348d071b97beb46899ca28200252da9805
David Boddie <david@boddie.org.uk>
2020-03-16 22:21:26 +0100

    Added a command line option to the bootstrap builder for the ROM bank to use.

--- build-bootstrap.sh
+++ build-bootstrap.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: `basename $0` <ROM file> <UEF file>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: `basename $0` <ROM file> <ROM number> <UEF file>"
     exit 1
 fi
+
+python -c 'import sys; n = sys.argv[1];
+open("asm/bootstrap-opts.oph", "w").write(
+    ".alias rom_number %s\n.alias ram_bank $fcd%i\n" % (n, int(n, 16) & 1))' "$2"
 
 ophis -o resources/temp asm/bootstrap.oph
 cp "$1" resources/temp2
@@ -21,5 +25,5 @@ python -c '
 open("resources/BOOTSTRAP.inf", "w").write("$.BOOTSTRAP 1000 1000 %x" % len(open("resources/BOOTSTRAP").read()))
 '
 
-UEFtrans.py "$2" new Electron 0
-UEFtrans.py "$2" append resources/BOOTSTRAP
+UEFtrans.py "$3" new Electron 0
+UEFtrans.py "$3" append resources/BOOTSTRAP
