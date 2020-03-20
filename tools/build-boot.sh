cf8814f56d303e00516f2df53b45c5005715ff85
David Boddie <david@boddie.org.uk>
2020-03-20 14:18:25 +0100

    Added a tool to boot ROM images by copying them into the sideways RAM on the MGC2, regardless of the cartridge slot (and ROM banks) in use, assuming that the MMFS ROM in use is resident in a bank on the MGC2.

--- tools/build-boot.sh
+++ tools/build-boot.sh
@@ -8,5 +8,9 @@ python tools/pad_to_page.py resources/temp
 cat resources/temp resources/TITLE > resources/splash.rom
 rm resources/temp
 
+ophis -o resources/LROM asm/loadrom.oph
+ophis -o resources/LROMS asm/loadroms.oph
+ophis -o resources/MROMS asm/mroms.oph
+
 rm -f disks/mgc2boot.ssd
 tools/dfs_add_files.py indices/mgc2boot.txt disks/mgc2boot.ssd
